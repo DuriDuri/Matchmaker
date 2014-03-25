0125e45f39452d9853a578a58c097430348134c6
Duri <durid@union.edu>
2014-03-24 20:15:25 -0400

    Test user profiles

--- src/matchmaker/urls.py
+++ src/matchmaker/urls.py
@@ -6,5 +6,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'profiles.views.home', name = 'Home'),
+    url(r'^$', 'profiles.views.all', name = 'Home'),
 )
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -5,5 +5,5 @@ def home(request):
     return render_to_response('home.html', locals(), context_instance=RequestContext(request))
 
 def all(request):
-    users = User.objects.filter(active=True)
+    users = User.objects.filter(is_active=True)
     return render_to_response('all.html', locals(), context_instance=RequestContext(request))
